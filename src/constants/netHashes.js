b313b4b4f7b71de9993122f320308d14ce5b907e
kanwong <kanwong@gmail.com>
2018-01-31 23:09:22 +0800

    update testnet hash

--- src/constants/netHashes.js
+++ src/constants/netHashes.js
@@ -1,5 +1,5 @@
 const netHash = {
-  testnet: 'a0c38f0dd302a6cf5ceef3d9dc5ccf0a12a34ab39c0cc42cf2d2602d8b44b0ad',
+  testnet: '52fd9a2f0857ca94510fae33c5e71b34eb5f9fa45813c5bbf6ee796c3731dbd9',
   mainnet: '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039',
 };
 
