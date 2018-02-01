1bac6f6c2ac126357bb92b81ac09543f55065887
Unhandled Exception <karek314@protonmail.ch>
2018-02-01 20:58:30 +0100

    Update netHashes.js

--- src/constants/netHashes.js
+++ src/constants/netHashes.js
@@ -1,6 +1,6 @@
 const netHash = {
-  testnet: '5e4182dfd5e50072651e0cdb75020c449941da694880fcea5aa4badf45e9e0d2',
-  mainnet: '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039',
+  testnet: '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b',
+  mainnet: '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b',
 };
 
 export default netHash;
