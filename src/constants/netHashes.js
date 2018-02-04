e0f4beec6d71970841b01b43b5896775b860a118
kanwong <kanwong@gmail.com>
2018-02-04 16:37:10 +0800

    finalized version and push it to new version 1.3.2 for testnet

--- src/constants/netHashes.js
+++ src/constants/netHashes.js
@@ -1,5 +1,5 @@
 const netHash = {
-  testnet: '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b',
+  testnet: 'aa14b4d84260e00b6fc033c022a25965629ab0e8a4aafc77e64cad4cf0dc2e00',
   mainnet: '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b',
 };
 
